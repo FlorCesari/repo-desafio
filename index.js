03743ff0d929916bc3902298c539f9481a7eb0cb
FlorCesari <cesariflorencia11@gmail.com>
2024-07-18 19:03:45 +0200

    otro cambio

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const ShuffledArr=_.shuffle(arr)
 console.log(ShuffledArr)
 console.log("otro cambio");
 console.log ("cambio numero dos");
+function main(){
+    console.log("un cambio");
+}
+main();
